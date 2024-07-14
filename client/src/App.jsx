2ca44ea0fa958db19e9d29b81ac7d2f41c09d5b1
shivam patel <shivamsinghpatel4597@gmail.com>
2024-07-14 09:32:13 +0530

    set Pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Project from './Pages/Project'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+
+import Dashboard from './Pages/Dashboard'
 
 function App() {
 
 
   return (
     <>
-     <h1 className='text-3xl text-red-400'>Mern blog application</h1>
+    <BrowserRouter>
+    <h1 className='text-3xl text-red-400'>Mern blog application</h1>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/signup" element={<Signup/>} />
+     <Route path="/project" element={<Project/>}  />
+      <Route path="/dashboard" element={<Dashboard/>} />
+    </Routes>
+    </BrowserRouter>
+    
+     
     </>
   )
 }
